33765b3d18c421423dece37cffea762a7882ea31
EM <em@EMs-MacBook-Pro.local>
2019-08-07 17:27:11 -0700

    more fxs

--- R/SingleR_Seurat.R
+++ R/SingleR_Seurat.R
@@ -91,3 +91,96 @@ SingleRmySerObj <- function(SeurObjPath = NULL, SeurObj = NULL, PlotFigs = T,
 
   
 }
+
+
+
+
+
+#' @title Save SingleR to SeurObj
+#'
+#' @description Add a pre-Computed SingleR classification into a Seurat object from path or direcly
+#' @param SeurObjPath, path to Seurat object
+#' @param SeurObj a Seurat object, but if path given, path is prioritized. 
+#' @param SingleRPath path to SingleR object
+#' @return modified Seurat object
+#' @keywords SingleR Classification 
+#' @export
+#' @import Seurat SingleR
+SaveSingleRtoSeurObj <- function(SeurObjPath = NULL, SeurObj = NULL, SingleRPath = NULL){
+  
+  if(is.null(SeurObj) & is.null(SeurObjPath)) stop("Seurat object not def, SeurObjPath or SeurObj needed.")
+  if(!is.null(SeurObjPath)) SeurObj <- readRDS(SeurObjPath)
+  
+  if(!is.null(SingleRPath)) singler <- readRDS(SingleRPath) else stop("SingleR path NULL")
+  
+  SeurObj$SingleR_Laels1 <- "unk"
+  SeurObj@meta.data[names(singler$singler[[1]]$SingleR.single$labels[,1]),]$SingleR_Laels1 <- singler$singler[[1]]$SingleR.single$labels[,1]
+  
+  table(SeurObj$SingleR_Laels1)
+  
+  
+  
+  
+  SeurObj$SingleR_Laels2 <- "unk"
+  SeurObj@meta.data[names(singler$singler[[2]]$SingleR.single$labels[,1]),]$SingleR_Laels2 <- singler$singler[[2]]$SingleR.single$labels[,1]
+  
+ 
+
+  return(SeurObj)
+  
+
+}
+
+
+#' @title DimPlot SingleR Class Lables
+#' @description Dimplot Seurobject with SingleR class lables
+#' @param SeurObj a Seurat object, but if path given, path is prioritized. 
+#' @keywords Dimplot SingleR Classification 
+#' @export
+#' @import Seurat SingleR cowplot
+DimPlot_SingleRClassLabs <- function(SeurObj){
+  cowplot::plot_grid(
+  DimPlot(SeurObj, group.by = "SingleR_Laels1") + theme_bw() + ggtitle("SingleR_Laels1") +theme(legend.position="bottom"),
+  DimPlot(SeurObj, group.by = "SingleR_Laels2") + theme_bw() + ggtitle("SingleR_Laels2") +theme(legend.position="bottom"), 
+  ncol = 1)
+  
+}
+
+
+#' @title Tabulate SingleR Class Lables
+#' @description Tabulate Seurobject with SingleR class lables
+#' @param SeurObj a Seurat object, but if path given, path is prioritized. 
+#' @keywords Tabulate SingleR Classification 
+#' @export
+#' @import Seurat SingleR cowplot
+Tabulate_SingleRClassLabs <- function(SeurObj){
+  cowplot::plot_grid(
+    ggplot(melt(table(SeurObj$SingleR_Laels1)), aes(x=Var1, y = value, fill=Var1))  + 
+      geom_bar(stat="identity", position="dodge", width = 0.7) + 
+      # scale_fill_manual(values=col_vector) +
+      theme_bw() + 
+      theme(legend.position="bottom",
+            legend.direction="horizontal",
+            legend.title = element_blank(),
+            axis.text.x = element_text(angle = 90)) +
+      ggtitle("SingleR predicted classification  labels #1:: TestisII \n Total Contribution") + 
+      ylab("Number of cells"),
+    
+    
+    ggplot(melt(table(SeurObj$SingleR_Laels2)), aes(x=Var1, y = value, fill=Var1))  + 
+      geom_bar(stat="identity", position="dodge", width = 0.7) + 
+      # scale_fill_manual(values=col_vector) +
+      theme_bw() + 
+      theme(legend.position="bottom",
+            legend.direction="horizontal",
+            legend.title = element_blank(),
+            axis.text.x = element_text(angle = 90)) +
+      ggtitle("SingleR predicted classification labels #2:: TestisII \n Total Contribution") + 
+      ylab("Number of cells"),
+    ncol = 1)
+  
+}
+
+
+
+
