3fa6e4ff8a117495766a508edf967a91d3549976
EM <em@EMs-MacBook-Pro.local>
2019-09-13 15:54:36 -0700

    new viz + debug

--- R/Viz.R
+++ R/Viz.R
@@ -109,7 +109,7 @@ PlotBin2D <- function(DF, bins = 75, ncol = 3){
 }
                       
 
-#' @title PlotBin2D
+#' @title PlotExprThrTab
 #'
 #' @description This function from a seurat object, takes the gene expression of a single gene and provides a vizualization for thresholding and corresponding descritized tabulation.
 #' @param SeuratObj, A Seurat object.
@@ -118,12 +118,15 @@ PlotBin2D <- function(DF, bins = 75, ncol = 3){
 #' @param TitleExtra, title from user.
 #' @return plot with ggplot tabulated bar plots. 
 #' @export
-ExprThrTab <- function(SeuratObj, GeneName = NULL, CutThresh = NULL, PlotBar = T,
+PlotExprThrTab <- function(SeuratObj, GeneName = NULL, CutThresh = NULL, PlotBar = T,
                        MetaDataName = NULL, TitleExtra = "", PlotHist = F, 
                        col_vector = col_vector){
   
   if(is.null(GeneName)) stop("GeneName is NULL")
-  if(is.null(MetaDataName)) stop("MetaDataName is NULL")
+  if(is.null(MetaDataName)) {
+    warning("MetaDataName is NULL, setting MetaDataName  = 'orig.ident' ")
+    MetaDataName  = "orig.ident"
+  }
   if(is.null(CutThresh)) stop("CutThresh is NULL")
   if(length(GeneName) != 1) stop ("GeneName Length != 1")
   
@@ -180,6 +183,140 @@ ExprThrTab <- function(SeuratObj, GeneName = NULL, CutThresh = NULL, PlotBar = T
       
       ggplot(melt(tempTab)) +
         geom_bar(aes(x=Var2, y=value, fill=factor(Var1)), stat="identity", width = 0.7, position="fill") +
+        theme_bw()  + scale_fill_manual(values=col_vector) +
+        theme(legend.position="bottom",
+              legend.direction="horizontal",
+              legend.title = element_blank(),
+              axis.text.x = element_text(angle = 90))+
+        scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                           labels = scales::percent(c(0, 0.25, 0.5, 0.75, 1))) +
+        ggtitle("\n Relative Contribution") + ylab("Relative % cells"),
+      
+      ncol = 2)
+    
+    print(p1)
+    
+    
+    
+  }
+  
+  
+  
+}
+
+
+#' @title PlotHistDensOptima
+#'
+#' @description This function plots histogram+density and finds all optima from a df usually from FetchData() of Seurat. 
+#' @param dftemp, a dataframe with column var1 to be plotted. 
+#' @param col_vector, color vector.
+#' @param nn, a color from color_vector.
+#' @return plot with ggplot tabulated bar plots. 
+#' @export
+PlotHistDensOptima <- function(dftemp, Print = F, col_vector = col_vector, nn = NULL, Title = ""){
+  dens <- density(dftemp$var1, n = 1000)
+  peaks <- dens$x[find_peaks(dens$y, m=20)]
+  dips <- dens$x[find_peaks(-1*dens$y, m=20)]
+  
+  if(is.null(nn)) nn=1
+  
+  
+  p1 <- ggplot(dftemp, aes(x=var1)) + 
+    geom_histogram(aes(y=..density..), colour="black", fill="white", binwidth = 0.05) +
+    geom_density(alpha=.2, fill=col_vector[nn]) +
+    geom_vline(aes(xintercept=mean(var1)), color="blue", linetype="dashed", size=.5) +
+    geom_vline(aes(xintercept=mean(var1) - 2*sd(var1)), color="dodgerblue", linetype="dashed", size=.5) +
+    geom_vline(aes(xintercept=mean(var1) + 2*sd(var1)), color="dodgerblue", linetype="dashed", size=.5) +
+    geom_vline(aes(xintercept=peaks[1] ), color="plum", size=.8) +
+    geom_vline(aes(xintercept=dips[1] ), color="red", size=1)  +
+    theme_bw() + ggtitle(Title)
+  
+  
+  if((length(peaks) >= 2)) p1 <- p1 + geom_vline(aes(xintercept=peaks[2] ), color="plum", size=.8)
+  if((length(peaks) >= 3)) p1 <- p1 + geom_vline(aes(xintercept=peaks[3] ), color="plum", size=.8)
+  if((length(peaks) >= 4)) p1 <- p1 + geom_vline(aes(xintercept=peaks[4] ), color="plum", size=.8)
+  
+  if((length(dips) >= 2)) p1 <- p1 + geom_vline(aes(xintercept=dips[2] ), color="brown", size=.8)
+  if((length(dips) >= 3)) p1 <- p1 + geom_vline(aes(xintercept=dips[3] ), color="brown", size=.8)
+  if((length(dips) >= 4)) p1 <- p1 + geom_vline(aes(xintercept=dips[4] ), color="brown", size=.8)
+  
+  if(Print) print(p1) else return(p1)
+}
+
+
+#' @title PlotFeatThrTab
+#'
+#' @description This function from a seurat object, takes the feature selected and provides a vizualization for thresholding and corresponding descritized tabulation.
+#' @param SeuratObj, A Seurat object.
+#' @param FeatName, the Feature name of interest. 
+#' @param CutThresh, cutting threshold.
+#' @param TitleExtra, title from user.
+#' @return plot with ggplot tabulated bar plots. 
+#' @export
+PlotFeatThrTab <- function(SeuratObj, FeatName = NULL, CutThresh = NULL, PlotBar = T,
+                       MetaDataName = NULL, TitleExtra = "", PlotHist = F, 
+                       col_vector = col_vector){
+
+  if(is.null(FeatName)) stop("FeatName is NULL")
+  if(is.null(MetaDataName)) {
+    warning("MetaDataName is NULL, setting MetaDataName  = 'orig.ident' ")
+    MetaDataName  = "orig.ident"
+  }
+  if(is.null(CutThresh)) stop("CutThresh is NULL")
+  if(length(FeatName) != 1) stop ("FeatName Length != 1")
+  
+  tempDF <- FetchData(SeuratObj, vars = c(FeatName, MetaDataName))
+  colnames(tempDF) <- c("var1", "var2")
+  tempTab <- table(tempDF$var1>CutThresh, tempDF$var2)
+  
+  
+  
+  print(addmargins(tempTab))
+  
+  if(!PlotHist){
+    if(PlotBar) p2 <- ggplot(data.table::melt(tempTab)) +
+        geom_bar(aes(x=factor(Var2), y=value, fill=factor(Var1)), stat="identity", width = 0.7) +
+        theme_bw()  + scale_fill_manual(values=col_vector) +
+        theme(legend.position="bottom",
+              legend.direction="horizontal",
+              legend.title = element_blank(),
+              axis.text.x = element_text(angle = 90)) +
+        ggtitle(paste0(FeatName, ">", CutThresh, " :: count", "\n", TitleExtra)) + ylab("Number of cells")
+    
+    print(p2)
+    
+  } else {
+    
+
+    
+    vp <- VlnPlot(SeuratObj, features = FeatName, 
+                  group.by = MetaDataName, cols = col_vector) + theme(legend.position = "none") +
+      geom_hline(aes(yintercept=CutThresh),
+                 color="blue", linetype="dashed", size=1)
+    
+    p1 <- cowplot::plot_grid(
+      ggplot(tempDF, aes(x=var1)) + 
+        geom_histogram(aes(y=..density..), colour="black", fill="white", binwidth = .03) +
+        geom_density(alpha=.2, fill="#E69F00") + theme_bw() + 
+        geom_vline(aes(xintercept=CutThresh ),
+                   color="blue", linetype="dashed", size=1) +
+        theme(legend.position="bottom",
+              legend.direction="horizontal",
+              legend.title = element_blank(),
+              axis.text.x = element_text(angle = 90)),
+      ggplot(data.table::melt(tempTab)) +
+        geom_bar(aes(x=factor(Var2), y=value, fill=factor(Var1)), stat="identity", width = 0.7) +
+        theme_bw()  + scale_fill_manual(values=col_vector) +
+        theme(legend.position="bottom",
+              legend.direction="horizontal",
+              legend.title = element_blank(),
+              axis.text.x = element_text(angle = 90)) +
+        ggtitle(paste0(FeatName, ">", CutThresh, " :: count", "\n", TitleExtra)) + ylab("Number of cells"),
+      
+      vp,
+      
+      ggplot(data.table::melt(tempTab)) +
+        geom_bar(aes(x=factor(Var2), y=value, fill=factor(Var1)), stat="identity", width = 0.7, position="fill") +
         theme_bw()  + scale_fill_manual(values=col_vector) +
         theme(legend.position="bottom",
               legend.direction="horizontal",
