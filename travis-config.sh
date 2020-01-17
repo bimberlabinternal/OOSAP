b4e49f909df2678613e493679664f4c2a384d36c
bbimber <bbimber@gmail.com>
2020-01-16 19:46:14 -0800

    Upgrade python

--- travis-config.sh
+++ travis-config.sh
@@ -10,6 +10,8 @@ mkdir $HOME/.R
 
 Rscript -e "install.packages(c('devtools', 'BiocManager', 'remotes'), dependencies=TRUE, ask = FALSE)"
 
+python --version
+
 echo -e 'CXX_STD = CXX14\n\nVER=\nCCACHE=ccache\nCC=$(CCACHE) gcc$(VER) -std=gnu99\nCXX=$(CCACHE) g++$(VER)\nC11=$(CCACHE) g++$(VER)\nC14=$(CCACHE) g++$(VER)\nFC=$(CCACHE) gfortran$(VER)\nF77=$(CCACHE) gfortran$(VER)\n' > $HOME/.R/Makevars
 echo 'max_size = 5.0G\n# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime\nsloppiness = include_file_ctime\n# also important as the (temp.) directory name will differ\nhash_dir = false' > ~/.ccache/ccache.conf
 
@@ -41,4 +43,4 @@ fi
 #echo "options(repos = BiocManager::repositories(version = 'devel'))" >> ~/.Rprofile.site
 
 echo 'R repos:'
-Rscript -e "getOption('repos')"
+Rscript -e "getOption('repos')"/
