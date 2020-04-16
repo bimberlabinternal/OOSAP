c5bb2b303aa992021479836589f025d8756a840b
bbimber <bbimber@gmail.com>
2020-04-16 13:00:08 -0700

    Revert travis to more closely match Dev branch

--- travis-config.sh
+++ travis-config.sh
@@ -8,32 +8,18 @@ echo 'Running travis setup'
 
 mkdir $HOME/.R
 
-Rscript -e "install.packages(c('devtools', 'BiocManager', 'remotes', 'shiny'), dependencies=TRUE, ask = FALSE)"
-
-echo "JAVA_HOME: "$JAVA_HOME
+Rscript -e "install.packages(c('devtools', 'BiocManager', 'remotes'), dependencies=TRUE, ask = FALSE)"
 
 # Force python3:
-echo 'alias python="python3.6"' >> ~/.bash_profile
-echo 'alias python3="python3.6"' >> ~/.bash_profile
-echo 'alias pip="pip3.6"' >> ~/.bash_profile
-echo 'alias pip3="pip3.6"' >> ~/.bash_profile
-source ~/.bash_profile
-
 python3.6 -m pip install --upgrade pip
 
-echo 'installed python packages:'
+python3 --version
 python --version
+pip3 --version
 pip --version
 
-which python
-which python3
-which python3.6
-which pip
-which pip3
-which pip3.6
-
-#echo -e 'CXX_STD = CXX14\n\nVER=\nCCACHE=ccache\nCC=$(CCACHE) gcc$(VER) -std=gnu99\nCXX=$(CCACHE) g++$(VER)\nC11=$(CCACHE) g++$(VER)\nC14=$(CCACHE) g++$(VER)\nFC=$(CCACHE) gfortran$(VER)\nF77=$(CCACHE) gfortran$(VER)\n' > $HOME/.R/Makevars
-#echo 'max_size = 5.0G\n# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime\nsloppiness = include_file_ctime\n# also important as the (temp.) directory name will differ\nhash_dir = false' > ~/.ccache/ccache.conf
+echo -e 'CXX_STD = CXX14\n\nVER=\nCCACHE=ccache\nCC=$(CCACHE) gcc$(VER) -std=gnu99\nCXX=$(CCACHE) g++$(VER)\nC11=$(CCACHE) g++$(VER)\nC14=$(CCACHE) g++$(VER)\nFC=$(CCACHE) gfortran$(VER)\nF77=$(CCACHE) gfortran$(VER)\n' > $HOME/.R/Makevars
+echo 'max_size = 5.0G\n# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime\nsloppiness = include_file_ctime\n# also important as the (temp.) directory name will differ\nhash_dir = false' > ~/.ccache/ccache.conf
 
 CORES=`Rscript -e "getOption('Ncpus', 1L)"`
 echo "Existing Ncpus: $CORES"
