5543aab572c56dfe40e51376d8b9733bb09b0962
kolabx <55809445+kolabx@users.noreply.github.com>
2020-07-16 22:19:47 -0700

    use FindClustersAndDimRedux instead

--- R/postSDA_processing.R
+++ R/postSDA_processing.R
@@ -13,58 +13,69 @@
 #' @param UMAP.NumEpoc UMAP min. distance. Passed directly to Seurat::RunUMAP()
 #' @import Seurat
 #' @export
-RecreateSeuratObjFromSDAmatrix <- function(cellnames = NULL, recomposedMat = NULL, metaDF, projectName = "SDAproject", dimsToUse = 15, spikeGenes = NULL, nfeatures = 500, 
-                                           tSNE_perplexity = 0.3, UMAP_MinDist = 0.3, UMAP_NumNeib = 35L, UMAP.NumEpoc = 500) {
-  
-  #confirm that gene are rows and columns are cells
-  if(any(grepl("CD1", rownames(recomposedMat)))) {
-    print("genes in rownames, good")
-  } else { recomposedMat <- t(recomposedMat) }
-  
+RecreateSeuratObjFromSDAmatrix <- function (cellnames = NULL, recomposedMat = NULL, metaDF, projectName = "SDAproject", 
+                                             minDimsToUse = 15, npcs = 50, 
+                                             spikeGenes = NULL, nVariableFeatures = 2000, variableFeatureSelectionMethod = "vst",
+                                             tSNE_perplexity = 30, tSNE_maxIter = 2000,
+                                             UMAP_NumNeib = 40L, UMAP_MinDist = 0.2, UMAP_Seed = 1234, UMAP.NumEpoc = 500,
+                                             mean.cutoff = c(0.0125, 3), dispersion.cutoff = c(0.5, Inf)) 
+{
+  if (any(grepl("CD1", rownames(recomposedMat)))) {
+    print("Confirmed that genes are in rows.")
+  }
+  else {
+    recomposedMat <- t(recomposedMat)
+  }
   recomposedMat <- recomposedMat[, cellnames]
-  
   recomposedMat <- as(recomposedMat, "dgCMatrix")
-  # recomposedMat <- recomposedMat + 10 #abs(round(min(recomposedMat)))+1
-  metaDF <-  as.data.frame(metaDF[which(metaDF$Barcode %in% cellnames), ])
-  
+  metaDF <- as.data.frame(metaDF[which(metaDF$Barcode %in% 
+                                         cellnames), ])
   rownames(metaDF) <- metaDF$Barcode
-  #genes should be in rows and cells in columns. 
+  seuratObj <- CreateSeuratObject(counts = recomposedMat, 
+                                  min.features = 10, project = projectName, meta.data = metaDF)
   
-  # with imputed full matrix, create seurat object 
-  seuratObj <- CreateSeuratObject(counts = recomposedMat, min.features = 10, project = projectName, meta.data = metaDF)
+  print("Normalizing data:")
+  seuratObj <- NormalizeData(object = seuratObj, normalization.method = "LogNormalize", 
+                             #scale.factor = 10000,
+                             verbose = T)
   
-  #TODO: if the imputed is pre-normalized is this redundant?
-  seuratObj <- NormalizeData(seuratObj, normalization.method = "LogNormalize", scale.factor = 10000)
-  tempData <- as.data.frame(seuratObj@assays$RNA@data)
-  tempData[is.na(tempData)] <- 0
-  tempData <- as(as.matrix(tempData), "dgCMatrix")
-  seuratObj@assays$RNA@data <- tempData
+  #drop NaN values in matrix (otherwise DE test errors)    
+  data <- as.data.frame(seuratObj@assays$RNA@data)
+  data[is.na(data)] <- 0
+  data <- as(as.matrix(data), "dgCMatrix")
+  seuratObj@assays$RNA@data <- data
   
   DefaultAssay(seuratObj) <- "RNA"
-  seuratObj <- FindVariableFeatures(object = seuratObj, selection.method="vst", nfeatures = nfeatures)
-  if(!is.null(spikeGenes)) {
-    seuratObj@assays$RNA@var.features <- unique(c(spikeGenes, seuratObj@assays$RNA@var.features))
+  
+  print("Find variable features:")
+  seuratObj <- FindVariableFeatures(object = seuratObj, selection.method = variableFeatureSelectionMethod, nfeatures = nVariableFeatures,
+                                    mean.cutoff=mean.cutoff, dispersion.cutoff=dispersion.cutoff, verbose = F)
+  
+  if (!is.null(spikeGenes)) {
+    VariableFeatures(seuratObj) <- unique(c(VariableFeatures(seuratObj), spikeGenes))
   }
   
-  # (is.na(SeurComboObj_SDA@assays$RNA@counts))
-  #NAs to 0, since some cells become NA by normalization step and mess things downstream for DE
-  seuratObj <- ScaleData(object = seuratObj, min.cells.to.block = 2000, features = rownames(seuratObj))
-  seuratObj <- RunPCA(object = seuratObj, npcs = 40)
+  seuratObj <- ScaleData(object = seuratObj, features = rownames(x = seuratObj), 
+                         #min.cells.to.block = 2000, 
+                         #vars.to.regress = c("nCount_RNA", "p.mito"), #slow computation: https://github.com/satijalab/seurat/issues/618
+                         verbose = F)
+  
+  seuratObj <- RunPCA(object = seuratObj, features = VariableFeatures(seuratObj), verbose = F, npcs = npcs)
+  
+  seuratObj <- ProjectDim(object = seuratObj)
+  
   ElbowPlot(seuratObj)
   
-  seuratObj <- FindNeighbors(object = seuratObj, dims = 1:dimsToUse)
-  seuratObj <- FindClusters(object = seuratObj, resolution = 0.1)
-  seuratObj <- FindClusters(object = seuratObj, resolution = 0.6)
-  seuratObj <- FindClusters(object = seuratObj, resolution = 1.2)
-  seuratObj <- RunTSNE(object = seuratObj, dims = 1:dimsToUse, perplexity = tSNE_perplexity, max_iter = 5000, num_threads = 10)
-  seuratObj <- RunUMAP(object = seuratObj, dims = 1:dimsToUse, n.neighbors = UMAP_NumNeib, min.dist = UMAP_MinDist, 
-                       metric = "correlation", seed.use = 11358, umap.method = "umap-learn", n.epochs = UMAP.NumEpoc)
+  print("Running FindClustersAndDimRedux:")
+  seuratObj <- OOSAP::FindClustersAndDimRedux(seuratObj = seuratObj, forceReCalc = FALSE, minDimsToUse = 15, 
+                                       maxTsneIter = tSNE_maxIter,
+                                       umap.method = "uwot", 
+                                       UMAP_NumNeib=UMAP_NumNeib, UMAP_MinDist=UMAP_MinDist, UMAP_Seed=UMAP_Seed, UMAP.NumEpoc=UMAP.NumEpoc)
   
   DimPlot(seuratObj)
   
   return(seuratObj)
 }
-
 
 
 
@@ -81,28 +92,30 @@ RecreateSeuratObjFromSDAmatrix <- function(cellnames = NULL, recomposedMat = NUL
 #' @param saveFile If provided, the seurat object will be saved as RDS to this location
 #' @import Seurat
 #' @export
-RecomposeSDAmatrix <- function(SDAseuratObj = NULL, compRemoveSet = "batch", 
-                               metaDF = NULL, spikeGenes = NULL, nfeatures = 2000, 
-                               tSNE_perplexity = 100, UMAP_MinDist = 0.5, UMAP_NumNeib = 60L, saveFile = NULL) {
-  
-  
+RecomposeSDAmatrix <- function(SDAseuratObj = NULL, compRemoveSet = "batch",  
+                               metaDF = NULL, projectName = 'imputedSeuratObj',
+                                spikeGenes = NULL, nVariableFeatures = 2000,  variableFeatureSelectionMethod = "vst",
+                                tSNE_perplexity = 30, tSNE_maxIter = 2000,
+                                UMAP_NumNeib = 40L, UMAP_MinDist = 0.2, UMAP_Seed = 1234, UMAP.NumEpoc = 500
+) 
+{
   if(! compRemoveSet %in% c("none", "qc", "batch")) {
     warning("compRemoveSet was not correctly parameterized: choose none, qc, or batch default to batch")
     compRemoveSet = "batch"
-  }
   
   SDAseuratObj$Barcode <- rownames(SDAseuratObj@meta.data)
-  
-  #combine SDAseuratObj metadata and raw seurat metadata 
-  if(!is.null(metaDF)){
-    if(!"Barcode" %in% colnames(metaDF)) 
+  if (!is.null(metaDF)) {
+    if (!"Barcode" %in% colnames(metaDF)) 
       metaDF$Barcode <- rownames(metaDF)
-    if("Barcode" %in% colnames(metaDF)){
-      metaDF <- merge(SDAseuratObj@meta.data, metaDF, by="Barcode", all = T, suffixes = c("SDA", "Seurat"))
-      if(nrow(metaDF) == nrow(SDAseuratObj@meta.data)){
+    if ("Barcode" %in% colnames(metaDF)) {
+      metaDF <- merge(SDAseuratObj@meta.data, metaDF, 
+                      by = "Barcode", all = T, suffixes = c(".SDA", 
+                                                            ".Seurat"))
+      if (nrow(metaDF) == nrow(SDAseuratObj@meta.data)) {
         rownames(metaDF) <- metaDF$Barcode
-        metaDF <- metaDF[rownames(SDAseuratObj@meta.data), ]
-      } else {
+        metaDF <- metaDF[rownames(SDAseuratObj@meta.data),]
+      }
+      else {
         metaDF <- SDAseuratObj@meta.data
       }
     }
@@ -110,33 +123,29 @@ RecomposeSDAmatrix <- function(SDAseuratObj = NULL, compRemoveSet = "batch",
     metaDF <- SDAseuratObj@meta.data
   }
   
-  #keep only passing componenets or all componenets
   if(compRemoveSet == "none") {
-    compsToKeep <- 1:ncol(SDAseuratObj@reductions$SDA@cell.embeddings)
+    compKeepSet <- SDAseuratObj@misc$SDA_processing_results$QC_components
   } else {
     if(compRemoveSet == "qc"){
-      compsToKeep <- setdiff(1:ncol(SDAseuratObj@reductions$SDA@cell.embeddings), as.numeric(SDAseuratObj@misc$SDA_processing_results$QC_components))
+      compKeepSet <- setdiff(1:ncol(SDAseuratObj@reductions$SDA@cell.embeddings), as.numeric(SDAseuratObj@misc$SDA_processing_results$QC_components))
     } else {
       if(compRemoveSet == "batch"){
-        compsToKeep <- setdiff(1:ncol(SDAseuratObj@reductions$SDA@cell.embeddings), as.numeric(SDAseuratObj@misc$SDA_processing_results$Remove_comps))
+        compKeepSet <- setdiff(1:ncol(SDAseuratObj@reductions$SDA@cell.embeddings), as.numeric(SDAseuratObj@misc$SDA_processing_results$Remove_comps))
       }
     }
   }
-    
-    
   
-  #do dot product of cell embeddings and gene loadings to get approximate/imputed raw seurat matrix
   print("Performing dot product...")
-  recomposedMat <-  t(SDAseuratObj@reductions$SDA@cell.embeddings[,compsToKeep] %*% t(SDAseuratObj@reductions$SDA@feature.loadings)[compsToKeep,])
+  recomposedMat <- t(SDAseuratObj@reductions$SDA@cell.embeddings[,compKeepSet] %*% t(SDAseuratObj@reductions$SDA@feature.loadings)[compKeepSet,])
   print("Finished dot product.")
   
-  if(saveFile=="" | is.null(saveFile)){ saveFile = "TempName" }
-  
-  #create seurat object with imputed matrix and run standard seuart processes
   print("Starting RecreateSeuratObjFromSDAmatrix...")
-  imputedSeuratObj <- RecreateSeuratObjFromSDAmatrix(cellnames = colnames(recomposedMat), recomposedMat = recomposedMat,  metaDF = metaDF, 
-                                                     projectName = gsub(pattern = ".rds", replacement = "", (saveFile)), spikeGenes = spikeGenes, 
-                                                     tSNE_perplexity = tSNE_perplexity, UMAP_MinDist = UMAP_MinDist, UMAP_NumNeib = UMAP_NumNeib, nfeatures = nfeatures)
+  imputedSeuratObj <- RecreateSeuratObjFromSDAmatrix(cellnames = colnames(recomposedMat), 
+                                                      recomposedMat=recomposedMat, metaDF=metaDF, projectName=projectName,
+                                                      spikeGenes = NULL, nVariableFeatures=nVariableFeatures, 
+                                                      tSNE_perplexity=tSNE_perplexity, tSNE_maxIter=tSNE_maxIter,
+                                                      UMAP_NumNeib=UMAP_NumNeib, UMAP_MinDist=UMAP_MinDist, UMAP_Seed=UMAP_Seed, UMAP.NumEpoc=UMAP.NumEpoc)
+  
   return(imputedSeuratObj)
 }
 
