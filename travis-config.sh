61b2b6368a04ef4cf372624c627cbeee01304ade
bbimber <bbimber@gmail.com>
2020-08-31 11:21:57 -0700

    Try to reduce logging during travisci build

--- travis-config.sh
+++ travis-config.sh
@@ -10,9 +10,6 @@ mkdir $HOME/.R
 
 export R_BIOC_VERSION='3.10'
 
-Rscript -e "install.packages(c('devtools', 'BiocManager', 'remotes'), dependencies=TRUE, ask = FALSE)"
-Rscript -e "BiocManager::install(version='${R_BIOC_VERSION}')"
-
 # Force python3:
 python3 -m pip install --upgrade pip
 
@@ -23,6 +20,11 @@ pip --version
 
 echo -e 'CXX_STD = CXX14\n\nVER=\nCCACHE=ccache\nCC=$(CCACHE) gcc$(VER) -std=gnu99\nCXX=$(CCACHE) g++$(VER)\nC11=$(CCACHE) g++$(VER)\nC14=$(CCACHE) g++$(VER)\nFC=$(CCACHE) gfortran$(VER)\nF77=$(CCACHE) gfortran$(VER)\n' > $HOME/.R/Makevars
 echo 'max_size = 5.0G\n# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime\nsloppiness = include_file_ctime\n# also important as the (temp.) directory name will differ\nhash_dir = false' > ~/.ccache/ccache.conf
+
+R CMD config CFLAGS
+CFLAGS=`R CMD config CFLAGS | sed 's/-Wall/-w/'`
+echo -e "CFLAGS=${CFLAGS}\n" >> $HOME/.R/Makevars
+cat $HOME/.R/Makevars
 
 CORES=`Rscript -e "getOption('Ncpus', 1L)"`
 echo "Existing Ncpus: $CORES"
@@ -35,7 +37,6 @@ echo "options(Ncpus = ${CORES})" >> ~/.Rprofile
 CORES=`Rscript -e "getOption('Ncpus', 1L)"`
 echo "Final Ncpus: $CORES"
 
-
 # Verify config, add Bioconductor repos to default repos, so install.packages will pick them up.
 if [ -e ~/.Rprofile ];then
     echo 'Rprofile exists:'
@@ -47,9 +48,14 @@ if [ -e ~/.Rprofile.site ];then
     cat ~/.Rprofile.site
 fi
 
+Rscript -e "install.packages(c('devtools', 'BiocManager', 'remotes'), dependencies=TRUE, ask = FALSE)"
+
 # See: https://stackoverflow.com/questions/26042751/cannot-install-package-xml-to-r
 echo "options(repos = c(BiocManager::repositories(version = '${R_BIOC_VERSION}'), c('xml' = 'http://www.omegahat.net/R')));" >> ~/.Rprofile.site
 
 # Log repos to ensure Bioconductor used:
 echo 'R repos:'
 Rscript -e "getOption('repos')"
+
+# Bioconductor
+Rscript -e "BiocManager::install(version='${R_BIOC_VERSION}')"
