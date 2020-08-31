97fb53df44704ac607dc2664117c5b2873b6f497
bbimber <bbimber@gmail.com>
2020-08-31 14:25:24 -0700

    Try to reduce logging during travisci build

--- travis-config.sh
+++ travis-config.sh
@@ -21,6 +21,9 @@ pip --version
 echo -e 'CXX_STD = CXX14\n\nVER=\nCCACHE=ccache\nCC=$(CCACHE) gcc$(VER) -std=gnu99\nCXX=$(CCACHE) g++$(VER)\nC11=$(CCACHE) g++$(VER)\nC14=$(CCACHE) g++$(VER)\nFC=$(CCACHE) gfortran$(VER)\nF77=$(CCACHE) gfortran$(VER)\n' > $HOME/.R/Makevars
 echo 'max_size = 5.0G\n# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime\nsloppiness = include_file_ctime\n# also important as the (temp.) directory name will differ\nhash_dir = false' > ~/.ccache/ccache.conf
 
+# See: https://stackoverflow.com/questions/49525561/rcppeigen-package-pragma-clang-diagnostic-pop-warnings
+echo -e 'PKG_CXXFLAGS=-w -DEIGEN_PERMANENTLY_DISABLE_STUPID_WARNINGS' >> $HOME/.R/Makevars
+
 CORES=`Rscript -e "getOption('Ncpus', 1L)"`
 echo "Existing Ncpus: $CORES"
 
