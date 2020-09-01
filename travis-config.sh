06c45b4eb68608248edfd8c26a4e98ab32ca38f4
bbimber <bbimber@gmail.com>
2020-08-31 21:59:57 -0700

    Update travis-ci config (#154)
    * Remove older hard-coded umap-learn versions, since bug is likely fixed: https://github.com/lmcinnes/umap/issues/209
    * Try to reduce logging during travisci build

--- travis-config.sh
+++ travis-config.sh
@@ -8,8 +8,7 @@ echo 'Running travis setup'
 
 mkdir $HOME/.R
 
-Rscript -e "install.packages(c('devtools', 'BiocManager', 'remotes'), dependencies=TRUE, ask = FALSE)"
-Rscript -e 'BiocManager::install(version="3.10")'
+export R_BIOC_VERSION='3.10'
 
 # Force python3:
 python3 -m pip install --upgrade pip
@@ -22,6 +21,27 @@ pip --version
 echo -e 'CXX_STD = CXX14\n\nVER=\nCCACHE=ccache\nCC=$(CCACHE) gcc$(VER) -std=gnu99\nCXX=$(CCACHE) g++$(VER)\nC11=$(CCACHE) g++$(VER)\nC14=$(CCACHE) g++$(VER)\nFC=$(CCACHE) gfortran$(VER)\nF77=$(CCACHE) gfortran$(VER)\n' > $HOME/.R/Makevars
 echo 'max_size = 5.0G\n# important for R CMD INSTALL *.tar.gz as tarballs are expanded freshly -> fresh ctime\nsloppiness = include_file_ctime\n# also important as the (temp.) directory name will differ\nhash_dir = false' > ~/.ccache/ccache.conf
 
+# See: https://stackoverflow.com/questions/49525561/rcppeigen-package-pragma-clang-diagnostic-pop-warnings
+addFlags() {
+    VAR=$1
+
+    VALUE=`R CMD config ${VAR}`
+    echo $VAR": "$VALUE
+    if [[ ${VALUE} != *"ERROR"* ]];then
+        echo -e $VAR"="${VALUE}" -w -pipe -pedantic -DEIGEN_PERMANENTLY_DISABLE_STUPID_WARNINGS\n" >> $HOME/.R/Makevars
+        R CMD config ${VAR}
+    fi
+}
+
+addFlags 'CXXFLAGS'
+addFlags 'CXX1XFLAGS'
+addFlags 'CXX11FLAGS'
+addFlags 'CXX14FLAGS'
+addFlags 'CXX17FLAGS'
+
+echo 'Makevars file:'
+cat $HOME/.R/Makevars
+
 CORES=`Rscript -e "getOption('Ncpus', 1L)"`
 echo "Existing Ncpus: $CORES"
 
@@ -32,7 +52,6 @@ CORES=2
 echo "options(Ncpus = ${CORES})" >> ~/.Rprofile
 CORES=`Rscript -e "getOption('Ncpus', 1L)"`
 echo "Final Ncpus: $CORES"
-
 
 # Verify config, add Bioconductor repos to default repos, so install.packages will pick them up.
 if [ -e ~/.Rprofile ];then
@@ -45,9 +64,14 @@ if [ -e ~/.Rprofile.site ];then
     cat ~/.Rprofile.site
 fi
 
+Rscript -e "install.packages(c('devtools', 'BiocManager', 'remotes'), dependencies=TRUE, ask = FALSE)"
+
 # See: https://stackoverflow.com/questions/26042751/cannot-install-package-xml-to-r
-echo "options(repos = c(BiocManager::repositories(version = '3.10'), c('xml' = 'http://www.omegahat.net/R')));" >> ~/.Rprofile.site
+echo "options(repos = c(BiocManager::repositories(version = '${R_BIOC_VERSION}'), c('xml' = 'http://www.omegahat.net/R')));" >> ~/.Rprofile.site
 
 # Log repos to ensure Bioconductor used:
 echo 'R repos:'
 Rscript -e "getOption('repos')"
+
+# Bioconductor
+Rscript -e "BiocManager::install(version='${R_BIOC_VERSION}')"
